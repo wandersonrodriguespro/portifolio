81b81881867510d62694b62e18ddf473ed61611b
Wanderson Rodrigues <wand_henrique@hotmail.com>
2023-11-22 20:14:58 -0300

    update portifolio

--- js/script.js
+++ js/script.js
@@ -27,3 +27,40 @@ function typeWriter(elemento) {
 const titulo = document.querySelector('.sobre p');
 
 typeWriter(titulo);
+
+// scroll
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav ul li a');
+    const scrollUpLink = document.querySelector('.scroll-up a');
+
+    links.forEach(link => {
+        link.addEventListener('click', smoothScroll);
+    });
+
+    scrollUpLink.addEventListener('click', smoothScrollToTop);
+
+    function smoothScroll(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scroll({
+            top: targetElement.offsetTop - document.querySelector('header').offsetHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    function smoothScrollToTop(e) {
+        e.preventDefault();
+
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+});
