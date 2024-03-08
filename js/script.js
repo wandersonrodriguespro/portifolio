bc22fd79bd4e151debc91ee4913b18cbcce5f934
Wanderson Rodrigues <wand_henrique@hotmail.com>
2024-03-08 20:38:40 -0300

    reorganização de pastas e remoção de arquivos

--- js/script.js
+++ js/script.js
@@ -64,3 +64,18 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+
+// Modal
+
+const closeModalButton = document.querySelector(".clouse-modal");
+const bgModal = document.querySelector(".bg-modal");
+const modal = document.querySelector(".modal-obrigado");
+
+const toggleModal = () => {
+  [modal, bgModal].forEach((el) => el.classList.toggle("hide"));
+};
+
+[closeModalButton, bgModal].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
